f22f1fab5ff01677a923607e4739868db6fd8cc9
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2025-02-15 21:40:14 -0800

    Peer type resolution: value and null

--- src/analysis.zig
+++ src/analysis.zig
@@ -2642,6 +2642,16 @@ pub const Type = struct {
                     return a;
                 }
             },
+            .ip_index => |a_payload| switch (a_payload.type) {
+                .null_type => switch (b.data) {
+                    .optional => return b,
+                    else => return .{
+                        .data = .{ .optional = try analyser.allocType(b.typeOf(analyser)) },
+                        .is_type_val = false,
+                    },
+                },
+                else => {},
+            },
             else => {},
         }
 
@@ -2650,6 +2660,16 @@ pub const Type = struct {
                 if (b_type.eql(a.typeOf(analyser))) {
                     return b;
                 }
+            },
+            .ip_index => |b_payload| switch (b_payload.type) {
+                .null_type => switch (a.data) {
+                    .optional => return a,
+                    else => return .{
+                        .data = .{ .optional = try analyser.allocType(a.typeOf(analyser)) },
+                        .is_type_val = false,
+                    },
+                },
+                else => {},
             },
             else => {},
         }
